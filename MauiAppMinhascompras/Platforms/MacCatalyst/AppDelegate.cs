3220aa9e57832b479716a5e35998ec8abc9dc3ea
Doni <Doni@DESKTOP-UOQ9IBP>
2025-02-24 11:28:10 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhascompras/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppMinhascompras;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
