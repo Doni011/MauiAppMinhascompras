4b98bb4c8f1721f226ceb7bf670c473cc4db6d30
Doni <adoniasguilherme10@hotmail.com>
2025-02-28 05:58:45 -0300

    ajustes

--- MauiAppMinhascompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhascompras/Helpers/SQLiteDatabaseHelper.cs
@@ -20,7 +20,7 @@ namespace MauiAppMinhascompras.Helpers
 
         public Task<List<Produto>> Update(Produto p)
         {
-            string sql = "UPDATE Produto SET Descricao= ?,Quantidade=? Preco=? WHERE id?";
+            string sql = "UPDATE Produto SET Descricao= ?, Quantidade=?, Preco=?, WHERE id?";
 
             return _conn.QueryAsync<Produto>(
                 sql, p.Descricao, p.Quantidade, p.Preco, p.Id
@@ -40,7 +40,7 @@ namespace MauiAppMinhascompras.Helpers
 
         public Task<List<Produto>> Search(string q) 
         {
-            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%' " ;
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%' " ;
 
             return _conn.QueryAsync<Produto>(
                 sql );
